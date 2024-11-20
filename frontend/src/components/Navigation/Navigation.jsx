8589cb5f4a5d3dc77dfbe9c5cf0932369698faa6
domutai <chenj68@gmail.com>
2024-11-19 20:31:11 -0500

    Finish Phase 5, might need to adjustProfile Dropdown Menu

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,53 +1,79 @@
+// import { NavLink } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import * as sessionActions from '../../store/session';
+// import OpenModalButton from '../OpenModalButton';
+// import LoginFormModal from '../LoginFormModal';
+// import SignupFormModal from '../SignupFormModal';
+// import './Navigation.css';
+
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector(state => state.session.user);
+//   const dispatch = useDispatch();
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+//   const sessionLinks = sessionUser ? (
+//     <>
+//       <li>
+//         <ProfileButton user={sessionUser} />
+//       </li>
+//       <li>
+//         <button onClick={logout}>Log Out</button>
+//       </li>
+//     </>
+//   ) : (
+//     <>
+//       <li>
+//         <OpenModalButton
+//           buttonText="Log In"
+//           modalComponent={<LoginFormModal />}
+//           />
+//       </li>
+//       <li>
+//         <OpenModalButton
+//           buttonText="Sign Up"
+//           modalComponent={<SignupFormModal />}
+//         />
+//       </li>
+//     </>
+//   );
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       {isLoaded && sessionLinks}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
+
+//Phase 5
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
-
-  const sessionLinks = sessionUser ? (
-    <>
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-      <li>
-        <button onClick={logout}>Log Out</button>
-      </li>
-    </>
-  ) : (
-    <>
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-          />
-      </li>
-      <li>
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    </>
-  );
 
   return (
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
