4a712b26a4d73780bb05a226456400415c1a5167
domutai <chenj68@gmail.com>
2024-12-02 11:42:06 -0300

    Deployment Try 5: Removed Dev Spot Mig

--- backend/db/migrations/20241105225220-create-spot.js
+++ backend/db/migrations/20241105225220-create-spot.js
@@ -1,86 +1,86 @@
 //SPOT DEVELOPMENT
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Spots', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      ownerId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'users', 
-          key: 'id', 
-        },
-        onDelete: 'CASCADE',
-      },
-      address: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      city: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      state: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      country: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      lat: {
-        type: Sequelize.DECIMAL(10, 8),
-        allowNull: false,
-      },
-      lng: {
-        type: Sequelize.DECIMAL(11, 8),
-        allowNull: false,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      price: {
-        type: Sequelize.DECIMAL(8, 2),
-        allowNull: false,
-      },
-      avgRating: {
-        type: Sequelize.DECIMAL(3, 2),
-        allowNull: false,
-        defaultValue: 0.0,
-      },
-      previewImage: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-    });
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.createTable('Spots', {
+//       id: {
+//         type: Sequelize.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false,
+//       },
+//       ownerId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false,
+//         references: {
+//           model: 'users', 
+//           key: 'id', 
+//         },
+//         onDelete: 'CASCADE',
+//       },
+//       address: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       city: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       state: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       country: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       lat: {
+//         type: Sequelize.DECIMAL(10, 8),
+//         allowNull: false,
+//       },
+//       lng: {
+//         type: Sequelize.DECIMAL(11, 8),
+//         allowNull: false,
+//       },
+//       name: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       description: {
+//         type: Sequelize.TEXT,
+//         allowNull: true,
+//       },
+//       price: {
+//         type: Sequelize.DECIMAL(8, 2),
+//         allowNull: false,
+//       },
+//       avgRating: {
+//         type: Sequelize.DECIMAL(3, 2),
+//         allowNull: false,
+//         defaultValue: 0.0,
+//       },
+//       previewImage: {
+//         type: Sequelize.STRING,
+//         allowNull: true,
+//       },
+//       createdAt: {
+//         type: Sequelize.DATE,
+//         allowNull: false,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//       },
+//       updatedAt: {
+//         type: Sequelize.DATE,
+//         allowNull: false,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//       },
+//     });
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Spots');
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.dropTable('Spots');
+//   },
+// };
 
 //SPOT PRODUCTION
 'use strict';
